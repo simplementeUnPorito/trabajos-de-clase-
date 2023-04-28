f2d61d90c0424b2ba2b8c5d35a63ed457c2c7bc2
GusZelaya <126886415+GusZelaya@users.noreply.github.com>
2023-04-28 11:54:13 -0400

    Create 2804.c

---
+++ 2804.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char *concatenar(char *a,int *l,int argc)
+{
+	int sum;
+	for (int i=0;i<strlen(a);i++)
+	{
+		*(l++)=strlen(*(a++));
+		sum+=strlen(*(a++));
+	}
+	char *b=malloc(sum*sizeof(*b));
+	for (int m=0;m<argc;m++)
+	{
+		for (int n=0;n<strlen(*(a+m));n++)
+		{
+				(b++)=(*(a+m));
+		}
+	}
+	return *b;
+}
+
+int main(int argc, char **argv){
+	int longitud[argc];
+for (int i=0;i<argc;i++)
+	printf("%s",*(argv+i));
+char *c=concatenar(*argv,longitud,argc);
+printf("resultado: %s/n",*c);
+printf("cantidad:");
+for (int k=0;k<argc;k++)
+	printf("%d",longitud[k]);
+}
