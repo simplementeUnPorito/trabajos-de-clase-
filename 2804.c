388f124ef33343380e9dac02f870f076da53f6fe
GusZelaya <126886415+GusZelaya@users.noreply.github.com>
2023-04-30 12:57:33 -0400

    Update 2804.c
    uwu

--- 2804.c
+++ 2804.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/*
+ * el trabajo de este programa es recivir los argumentos que carga el usuario, meterlos todos dentro de una cadena e imprimirla
+ * ademas tambien debe imprimir la cantidad de carateres de cada argumento 
+ */
 
-void concatenar(char *a,int *l,int argc,char *c)
+/*
+ * funcion encargada de concatenar
+ * **a--> argv (puntero a vector de punteros)
+ * *l--> matriz en la que se quiere guardar la longitud de cada argumento
+ * argc--> cantidad de argumentos
+ * *c--> la intencion es que sea utilizado para hacer la string (fernando pidio que lo hagamos asi para practicar con la memoria dinamica)
+ */
+void concatenar(char **a,int *l,int argc,char *c)
 {
-	int sum;
-	for (int i=0;i<strlen(a++);i++)
+	int sum=0;
+	for (int i=0;i<strlen(a[i]);i++)//en esta parte se carga en l la cantidad de caracteres de cada argumento
 	{
-		l[i]=strlen(a++);
+		l[i]=strlen(a[i]);
 		sum+=l[i];
 	}
-    c=malloc(sum*sizeof(char));
-	for (int m=0;m<argc;m++)
+    c=malloc(sum*sizeof(char));		//se reserva el espacio en c para almacenar todos los caracteres de las cadenas ingresadas 
+	for (int m=0;m<argc;m++)	//para recorrer todos los argumentos 
 	{
-		for (int n=0;n<strlen(a++);n++)
+		for (int n=0;n<strlen(a[n]);n++)//para recorrer por cada caracter del argumento correspondiente 	
 		{
-				c[m]=a[m];
+				c[m]=*a[m];			//se cargan los caracteres 
 		}
 	}
 }
@@ -25,10 +36,12 @@ int main(int argc, char **argv){
 for (int i=0;i<argc;i++)
 	printf("%s",argv[i]);
 char *c;
-concatenar(argv,longitud,argc,c);
-printf("resultado: %s/n",c);
+concatenar(argv,longitud,argc,&c);
+printf("\n");
+printf("resultado: %s",c);
+printf("\n");
 printf("cantidad:");
 free(c);
 for (int k=0;k<argc;k++)
-	printf("%d",longitud[k]);
+	printf("%d,",longitud[k]);
 }
