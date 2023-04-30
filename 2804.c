7b86e682de36b4fcb37fb66123456fc57bb6fa9a
simplementeUnPorito <111580856+simplementeUnPorito@users.noreply.github.com>
2023-04-30 17:43:34 -0400

    Update 2804.c

--- 2804.c
+++ 2804.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 /*
- * el trabajo de este programa es recivir los argumentos que carga el usuario, meterlos todos dentro de una cadena e imprimirla
- * ademas tambien debe imprimir la cantidad de carateres de cada argumento 
+ * el trabajo de este programa es recibir los argumentos que carga el usuario, meterlos todos dentro de una cadena e imprimirla
+ * ademas tambien debe imprimir la cantidad de carateres de cada argumento
  */
 
 /*
@@ -13,35 +13,43 @@
  * argc--> cantidad de argumentos
  * *c--> la intencion es que sea utilizado para hacer la string (fernando pidio que lo hagamos asi para practicar con la memoria dinamica)
  */
-void concatenar(char **a,int *l,int argc,char *c)
+
+typedef struct dato{
+	char* string;
+	int* cant;
+}dato;
+
+dato concatenar(char **a,int argc, datos respuesta)
 {
 	int sum=0;
-	for (int i=0;i<strlen(a[i]);i++)//en esta parte se carga en l la cantidad de caracteres de cada argumento
+
+	respuesta.cant=(int*)malloc(argc*sizeof(int));
+
+	for (int i=0;i<argc;i++)//en esta parte se carga en l la cantidad de caracteres de cada argumento
 	{
-		l[i]=strlen(a[i]);
-		sum+=l[i];
+		respuesta.cant[i]=strlen(a[i]);
+		sum+=respuesta.cant[i];
 	}
-    c=malloc(sum*sizeof(char));		//se reserva el espacio en c para almacenar todos los caracteres de las cadenas ingresadas 
-	for (int m=0;m<argc;m++)	//para recorrer todos los argumentos 
+    respuesta.string=malloc(sum*sizeof(char));		//se reserva el espacio en c para almacenar todos los caracteres de las cadenas ingresadas
+    (respuesta.string)[0]= '\0';
+    for (int m=0;m<argc;m++)	//para recorrer todos los argumentos
 	{
-		for (int n=0;n<strlen(a[n]);n++)//para recorrer por cada caracter del argumento correspondiente 	
-		{
-				c[m]=*a[m];			//se cargan los caracteres 
-		}
+		strcat(respuesta.string,a[m]);
 	}
+
+    return respuesta;
+
 }
 
+
+
+
 int main(int argc, char **argv){
-	int longitud[argc];
-for (int i=0;i<argc;i++)
-	printf("%s",argv[i]);
-char *c;
-concatenar(argv,longitud,argc,&c);
-printf("\n");
-printf("resultado: %s",c);
-printf("\n");
-printf("cantidad:");
-free(c);
-for (int k=0;k<argc;k++)
-	printf("%d,",longitud[k]);
+
+	dato respuesta;
+	respuesta  = concatenar(argv,argc,respuesta);
+	printf("Resultado: %s\nChars/Arg:\n",respuesta.string);
+	for(int i=0;i<argc;i++)
+		printf("\tArg[%d]: %d\n",i,respuesta.cant[i]);
+
 }
